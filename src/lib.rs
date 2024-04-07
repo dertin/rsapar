1a897e35e1fe6d99e1ecaaf3ba09152446548643
Guillermo Céspedes Tabárez <1295883+dertin@users.noreply.github.com>
2024-04-07 10:06:39 -0300

    Added DecimalFormat for validator (#3)
    * initial commit
    
    * Added DecimalFormat for validator

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod parser;
 mod schema;
+mod decimal_format;
 
 pub fn example() {
     let parser = parser::Parser::new(parser::ParserConfig {
@@ -32,5 +33,32 @@ mod tests {
         });
         let result = parser.start();
         assert!(result.is_ok(), "ERROR: {:?}", result.unwrap_err());
+    }
+
+    #[test]
+    fn test_validate_number() {
+
+        // in development
+
+        // # is optional digit, 0 is required digit
+        let pattern = "0,##0.00;(#,##0.000)"; 
+        let formatter = decimal_format::DecimalFormat::new(pattern).unwrap();
+        assert!(formatter.validate_number("2,234.56").is_ok());
+        assert!(formatter.validate_number("-1,234.560").is_ok());
+        assert!(formatter.validate_number("1234.56").is_ok());
+        assert!(formatter.validate_number("1234").is_err());
+
+        let pattern = "0.#0,##0";
+        let formatter = decimal_format::DecimalFormat::new(pattern).unwrap();
+        assert!(formatter.validate_number("2.20,125").is_ok());
+        // ' is a literal character for special characters
+        let pattern = "';#'##0";
+        let formatter = decimal_format::DecimalFormat::new(pattern).unwrap();
+        assert!(formatter.validate_number(";#123").is_ok());
+
+        let pattern = "#######0.00";
+        let formatter = decimal_format::DecimalFormat::new(pattern).unwrap();
+        assert!(formatter.validate_number("00000000.00").is_ok());
+        
     }
 }
