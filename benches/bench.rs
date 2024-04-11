c35157aa38f79dd160148c9d5a3cf4e005c29a1e
Guillermo Céspedes Tabárez <1295883+dertin@users.noreply.github.com>
2024-04-11 12:30:28 -0300

    Added benchmark and performance has improved for DecimalFormat. Set MSRV 1.70.0 (#14)

---
+++ benches/bench.rs
@@ -0,0 +1,21 @@
+use core::panic;
+
+use rsapar::DecimalFormat;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn bench_decimal_format_new(c: &mut Criterion) {
+    c.bench_function("decimal_format_new", |b| {
+        b.iter(|| {
+            let pattern = "0,##0.00;(#,##0.000)";
+            let formatter = DecimalFormat::new(pattern).unwrap();
+            assert!(formatter.validate_number("2,234.56").is_ok());
+            assert!(formatter.validate_number("-1,234.560").is_ok());
+            assert!(formatter.validate_number("1234.56").is_err());
+            assert!(formatter.validate_number("1234").is_err());
+        })
+    });
+}
+
+criterion_group!(benches, bench_decimal_format_new,);
+criterion_main!(benches);
